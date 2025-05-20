69e8ac50d653a0b5a1fca3d635e5ff9d7728b733
Adenaike David <coledav555@gmail.com>
2025-05-20 09:31:47 +0100

    commit initial

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+Addition = (number1 + number2)
+Subtraction = (number1 - number2)
+Multiplication = (number1 * number2)
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+# This code performs basic arithmetic operations on Addition.
+# This code performs basic arithmetic operations on Subtraction.
+# This code performs basic arithmetic operations on Multiplication. 
