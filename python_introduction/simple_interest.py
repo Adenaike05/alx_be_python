88817d4b7cee95ab44bf1d17e53ebe83f2b8a312
Adenaike David <coledav555@gmail.com>
2025-05-20 09:40:59 +0100

    commit initial

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,11 @@
 P = int(1000)
 R = float(0.05)
-T = 3
+T = int(3)
 I = P * R * T
 print("The simple interest is " + str(I))
 # This code calculates the simple interest using the formula I = P * R * T.
 # where:
-# ( I ) is the interest earned,
-# ( P ) is the principal amount (initial investment),
-# ( R ) is the annual interest rate (as a decimal),
-# ( T ) is the time the money is invested for in years.
+# I is the interest earned,
+# P is the principal amount (initial investment),
+# R is the annual interest rate (as a decimal),
+# T is the time the money is invested for in years.
