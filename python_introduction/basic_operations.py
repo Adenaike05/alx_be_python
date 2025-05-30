73863dd1344c6a884057209d17b3e9aa89dbf6f6
Adenaike David <coledav555@gmail.com>
2025-05-30 10:34:14 +0100

    commit initial

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,11 @@
+# Define variables
+int = "number1"
+int = "number2"         
 number1 = 10
 number2 = 5
-Addition = (number1 + number2)
-Subtraction = (number1 - number2)
-Multiplication = (number1 * number2)
+Addition = int(number1 + number2)
+Subtraction = int(number1 - number2)
+Multiplication = int(number1 * number2)
 print(Addition)
 print(Subtraction)
 print(Multiplication)
